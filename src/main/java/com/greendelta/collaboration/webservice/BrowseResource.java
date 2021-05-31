e13e61f22bdc41ad2c23eca054bf2ff9d46027b9
Sebastian Greve <greve@greendelta.com>
2021-05-31 16:49:04 +0200

    Optimized descriptor loading

--- src/main/java/com/greendelta/collaboration/webservice/BrowseResource.java
+++ src/main/java/com/greendelta/collaboration/webservice/BrowseResource.java
@@ -23,15 +23,17 @@ import org.openlca.cloud.api.git.Reference;
 import org.openlca.cloud.api.git.References.Entry;
 import org.openlca.cloud.api.git.References.EntryType;
 import org.openlca.core.model.ModelType;
+import org.openlca.util.Strings;
 
-import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.google.inject.Inject;
 import com.greendelta.collaboration.model.User;
+import com.greendelta.collaboration.model.settings.ServerSetting;
 import com.greendelta.collaboration.service.Repository;
 import com.greendelta.collaboration.service.RepositoryService;
+import com.greendelta.collaboration.service.SettingsService;
 import com.greendelta.collaboration.service.user.UserService;
 import com.greendelta.collaboration.util.ObjectMap;
 import com.greendelta.collaboration.util.SearchResults;
@@ -43,11 +45,13 @@ public class BrowseResource {
 	private static final Logger log = LogManager.getLogger(BrowseResource.class);
 	private final RepositoryService repoService;
 	private final UserService userService;
+	private final SettingsService settingsService;
 
 	@Inject
-	public BrowseResource(RepositoryService repoService, UserService userService) {
+	public BrowseResource(RepositoryService repoService, UserService userService, SettingsService settingsService) {
 		this.repoService = repoService;
 		this.userService = userService;
+		this.settingsService = settingsService;
 	}
 
 	@GET
@@ -64,10 +68,20 @@ public class BrowseResource {
 		log.debug("Getting content for path /{} of repository {}/{}", path, group, name);
 		Repository repo = repoService.get(group, name);
 		List<Entry> entries = repo.references.walk(commitId, categoryPath);
-		// TODO this is really slow for most categories
+		List<String> typesOrder = settingsService.get(ServerSetting.MODEL_TYPES_ORDER);
+		if (path.isEmpty()) {
+			List<String> typesHidden = settingsService.get(ServerSetting.MODEL_TYPES_HIDDEN);
+			entries = com.greendelta.collaboration.util.Collections.filter(entries,
+					e -> typesHidden.contains(e.type.name()));
+		}
 		List<ObjectMap> mapped = putInfo(entries, repo, commitId, categoryPath);
-		Collections.sort(mapped,
-				(m1, m2) -> m1.getString("name").toLowerCase().compareTo(m2.getString("name").toLowerCase()));
+		Collections.sort(mapped, (m1, m2) -> {
+			if (!path.isEmpty())
+				return m1.getString("name").toLowerCase().compareTo(m2.getString("name").toLowerCase());
+			String t1 = m1.getString("type");
+			String t2 = m2.getString("type");
+			return Integer.compare(typesOrder.indexOf(t1), typesOrder.indexOf(t2));
+		});
 		return Respond.ok(SearchResults.pagedAndFiltered(page, pageSize, filter, mapped, m -> m.getString("name")));
 	}
 
@@ -79,11 +93,11 @@ public class BrowseResource {
 		List<ObjectMap> mapped = new ArrayList<>();
 		entries.forEach(entry -> {
 			ObjectMap map = ObjectMap.fromObject(entry);
-			String entryPath = Strings.isNullOrEmpty(categoryPath)
+			String entryPath = Strings.nullOrEmpty(categoryPath)
 					? entry.name
 					: categoryPath + "/" + entry.name;
 			if (loggedIn) {
-				putCommitInfo(map, entry, repo, commits, entryPath);
+				putCommitInfo(map, entry, repo, commits);
 			}
 			if (entry.typeOfEntry != EntryType.DATASET) {
 				map.put("count", repo.references.find().commit(commitId).path(entryPath).count());
@@ -95,19 +109,20 @@ public class BrowseResource {
 		return mapped;
 	}
 
-	private void putCommitInfo(Map<String, Object> map, Entry entry, Repository repo, Map<String, Commit> commits,
-			String entryPath) {
-		String entryCommitId = repo.commits.find().path(entryPath).latestId();
-		Commit commit = commits.computeIfAbsent(entryCommitId, repo.commits::get);
+	private void putCommitInfo(Map<String, Object> map, Entry entry, Repository repo, Map<String, Commit> commits) {
+		Commit commit = commits.computeIfAbsent(entry.commitId, repo.commits::get);
 		map.put("commitId", commit.id);
 		map.put("commitMessage", commit.message);
 		map.put("commitTimestamp", commit.timestamp);
 	}
 
 	private void putDatasetInfo(Map<String, Object> map, Entry entry, Repository repo, String commitId) {
-		Reference ref = repo.references.get(entry.type, entry.refId, commitId);
-		Descriptor descriptor = repo.datasets.getDescriptor(ref);
-		map.put("name", descriptor.name);
+		Descriptor descriptor = repo.datasets.getDescriptor(entry);
+		if (Strings.nullOrEmpty(descriptor.location)) {
+			map.put("name", descriptor.name);
+		} else {
+			map.put("name", descriptor.name + " - " + descriptor.location);
+		}
 		if (descriptor.flowType != null) {
 			map.put("flowType", descriptor.flowType);
 		}
@@ -137,7 +152,7 @@ public class BrowseResource {
 			return Respond.unauthorized();
 		Reference ref = repo.references.get(type, refId, commit.id);
 		String dataset = repo.datasets.get(ref);
-		if (Strings.isNullOrEmpty(dataset))
+		if (Strings.nullOrEmpty(dataset))
 			return Respond.notFound(type + " " + refId + " not found for commit " + commitId);
 		log.info("Loading {} {} of repository {}/{} (commit id: {})", type, refId, group, name, commitId);
 		JsonObject json = new Gson().fromJson(dataset, JsonObject.class);
