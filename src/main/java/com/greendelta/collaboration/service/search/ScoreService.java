382d2ddc59004d9ca9e31caa4694ee27203fa1df
Sebastian Greve <greve@greendelta.com>
2023-06-19 11:38:57 +0200

    Removed obsolete repository ordering from search score

--- src/main/java/com/greendelta/collaboration/service/search/ScoreService.java
+++ src/main/java/com/greendelta/collaboration/service/search/ScoreService.java
@@ -3,9 +3,7 @@ package com.greendelta.collaboration.service.search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.greendelta.collaboration.service.RepositoryService;
 import com.greendelta.collaboration.service.SettingsService;
-import com.greendelta.collaboration.service.user.UserService;
 import com.greendelta.collaboration.util.Aggregations;
 import com.greendelta.search.wrapper.SearchQueryBuilder;
 import com.greendelta.search.wrapper.score.Comparator;
@@ -14,34 +12,17 @@ import com.greendelta.search.wrapper.score.Score;
 @Service
 class ScoreService {
 
-	private final RepositoryService repoService;
 	private final SettingsService settings;
-	private final UserService userService;
 
 	@Autowired
-	ScoreService(RepositoryService repoService, SettingsService settings, UserService userService) {
-		this.repoService = repoService;
+	ScoreService(SettingsService settings) {
 		this.settings = settings;
-		this.userService = userService;
 	}
 
 	void applyTo(SearchQueryBuilder builder) {
-		var currentUser = userService.getCurrentUser();
-		if (currentUser.isAnonymous()) {
-			applyRepositoryOrder(builder);
-		}
 		applyTypeOrder(builder);
 	}
 
-	private void applyRepositoryOrder(SearchQueryBuilder builder) {
-		var score = new Score(Aggregations.REPOSITORY.field);
-		var repoOrder = repoService.getPublicRepositoryOrder();
-		for (var i = 0; i < repoOrder.size(); i++) {
-			score.addCase(repoOrder.size() - i + 1, Comparator.EQUALS, "\"" + repoOrder.get(i) + "\"");
-		}
-		score.addElse(1);
-		builder.score(score);
-	}
 
 	private void applyTypeOrder(SearchQueryBuilder builder) {
 		var score = new Score(Aggregations.MODEL_TYPE.field);
