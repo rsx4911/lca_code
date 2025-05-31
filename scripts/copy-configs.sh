0f81a46bb3457197b7067b4795204090350d7fac
Ibin Issac (Fintrens) <137661858+ibin-fintrens@users.noreply.github.com>
2025-05-31 09:37:41 +0530

    Updates

--- scripts/copy-configs.sh
+++ scripts/copy-configs.sh
@@ -6,6 +6,4 @@ echo "Copying config files for environment: $ENV"
 
 cp configs/nginx/$ENV.nginx.conf /etc/nginx/nginx.conf
 
-#More configuration based upon the environment needs to be introduced
-
 echo "Configuration files copied successfully!"
