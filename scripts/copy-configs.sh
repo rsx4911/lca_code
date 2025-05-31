f020735b3e288255f62d06b46d023846059984ee
Ibin Issac (Fintrens) <137661858+ibin-fintrens@users.noreply.github.com>
2025-05-31 09:25:15 +0530

    Updates

--- scripts/copy-configs.sh
+++ scripts/copy-configs.sh
@@ -5,8 +5,6 @@ ENV=$1
 echo "Copying config files for environment: $ENV"
 
 cp configs/nginx/$ENV.nginx.conf /etc/nginx/nginx.conf
-cp configs/tomcat/$ENV.server.xml /opt/tomcat/conf/server.xml
-cp configs/opensearch/$ENV.opensearch.yml /etc/opensearch/opensearch.yml
 
 #More configuration based upon the environment needs to be introduced
 
